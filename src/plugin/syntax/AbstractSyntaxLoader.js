e163c32bbdf899b61cdb0d5ee5cca2eff7d69d35
typhonrt <support@typhonrt.org>
2016-05-30 23:43:48 -0700

    added AbstractSyntaxLoader

---
+++ src/plugin/syntax/AbstractSyntaxLoader.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Provides the base implementation for all syntax loader plugins which automatically associates member methods
+ */
+export default class AbstractSyntaxLoader
+{
+   /**
+    * Loads all member methods including from child classes that are not `constructor` or `onLoadSyntax`.
+    *
+    * @param {object}   ev - escomplex plugin event data.
+    */
+   onLoadSyntax(ev)
+   {
+      const syntaxes = {};
+
+      for (let name of Object.getOwnPropertyNames(Object.getPrototypeOf(this)))
+      {
+         // Skip constructor & onLoadSyntax methods.
+         if (!(this[name] instanceof Function) || name === 'constructor' || name === 'onLoadSyntax') { continue; }
+
+         syntaxes[name] = this[name](ev.data.settings);
+      }
+
+      ev.data.syntaxes = syntaxes;
+   }
+}
