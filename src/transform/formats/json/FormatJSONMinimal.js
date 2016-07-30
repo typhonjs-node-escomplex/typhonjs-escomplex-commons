24a55097add79524811f96035aa058695171d4a7
typhonrt <support@typhonrt.org>
2016-07-30 07:23:11 -0700

    updated FormatJSONMinimal for universal output

--- src/transform/formats/json/FormatJSONMinimal.js
+++ src/transform/formats/json/FormatJSONMinimal.js
@@ -24,75 +24,34 @@ export default class FormatJSONMinimal
     */
    formatReport(report, options = {})
    {
+      let localOptions = Object.assign({}, this._keys);
+      localOptions = Object.assign(localOptions, options);
+
+      let output;
+
       switch (report.type)
       {
          case ReportType.CLASS:
+            output = this._formatClass(report, localOptions);
             break;
 
          case ReportType.CLASS_METHOD:
          case ReportType.MODULE_METHOD:
+            output = this._formatMethod(report, localOptions);
             break;
 
          case ReportType.MODULE:
-            return this.formatModule(report, options);
+            output = this._formatModule(report, true, localOptions);
+            break;
 
          case ReportType.PROJECT:
-            return this.formatProject(report, options);
+            output = this._formatProject(report, localOptions);
+            break;
 
          default:
             console.warn(`formatReport '${this.name}' warning: unsupported report type '${report.type}'.`);
             return '';
       }
-   }
-
-   /**
-    * Formats a module report as a JSON string.
-    *
-    * @param {ModuleReport}   report - A module report.
-    *
-    * @param {object}         options - (Optional) One or more optional parameters passed to the formatter.
-    * @property {string}      classReport - An entry key found in the class report to output.
-    * @property {string}      methodReport - An entry key found in the method report to output.
-    * @property {string}      moduleReport - An entry key found in the module report to output.
-    *
-    * @returns {string}
-    */
-   formatModule(report, options = {})
-   {
-      let localOptions = Object.assign({}, this._keys);
-      localOptions = Object.assign(localOptions, options);
-
-      const output = this._formatModule(report, true, localOptions);
-
-      return typeof localOptions === 'object' && Number.isInteger(localOptions.spacing) ?
-       JSON.stringify(output, void 0, localOptions.spacing) : JSON.stringify(output);
-   }
-
-   /**
-    * Formats a project result as minimal / plain text.
-    *
-    * @param {ProjectResult}  result - A project result.
-    *
-    * @param {object}         options - (Optional) One or more optional parameters passed to the formatter.
-    * @property {string}      classReport - An entry key found in the class report to output.
-    * @property {string}      methodReport - An entry key found in the method report to output.
-    * @property {string}      moduleReport - An entry key found in the module report to output.
-    *
-    * @returns {string}
-    */
-   formatProject(result, options = {})
-   {
-      let localOptions = Object.assign({}, this._keys);
-      localOptions = Object.assign(localOptions, options);
-
-      const output = { modules: [] };
-
-      const reportsAvailable = result.getSetting('serializeReports', false);
-
-      result.reports.forEach((report) =>
-      {
-         output.modules.push(this._formatModule(report, reportsAvailable, localOptions));
-      });
 
       return typeof localOptions === 'object' && Number.isInteger(localOptions.spacing) ?
        JSON.stringify(output, void 0, localOptions.spacing) : JSON.stringify(output);
@@ -154,77 +113,64 @@ export default class FormatJSONMinimal
    /**
     * Formats a module reports methods array.
     *
-    * @param {Array<ClassReport>}   classReports - An array of ClassReport instances to format.
+    * @param {ClassReport} classReport - A ClassReport instance to format.
     *
-    * @param {object}               options - (Optional) One or more optional parameters passed to the formatter.
-    * @property {string}            classReport - An entry key found in the class report to output.
-    * @property {string}            methodReport - An entry key found in the method report to output.
+    * @param {object}      options - (Optional) One or more optional parameters passed to the formatter.
+    * @property {string}   classReport - An entry key found in the class report to output.
+    * @property {string}   methodReport - An entry key found in the method report to output.
     *
-    * @returns {Array<object>}
+    * @returns {object}
     */
-   _formatClasses(classReports, options)
+   _formatClass(classReport, options)
    {
-      const output = [];
+      const entry = {};
 
-      classReports.forEach((classReport) =>
+      if (classReport.name) { entry.name = classReport.name; }
+      if (classReport.lineStart) { entry.lineStart = classReport.lineStart; }
+      if (classReport.lineEnd) { entry.lineEnd = classReport.lineEnd; }
+
+      if (Array.isArray(options.classReport))
       {
-         const entry = {};
-
-         if (classReport.name) { entry.name = classReport.name; }
-         if (classReport.lineStart) { entry.lineStart = classReport.lineStart; }
-
-         if (Array.isArray(options.classReport))
+         options.classReport.forEach((classEntry) =>
          {
-            options.classReport.forEach((classEntry) =>
-            {
-               const entryValue = ObjectUtil.safeAccess(classReport, classEntry);
-               if (entryValue) { entry[classEntry] = entryValue; }
-            });
-         }
+            const entryValue = ObjectUtil.safeAccess(classReport, classEntry);
+            if (entryValue) { ObjectUtil.safeSet(entry, classEntry, entryValue); }
+         });
+      }
 
-         entry.methods = this._formatMethods(classReport.methods, options);
+      entry.methods = classReport.methods.map((methodReport) => this._formatMethod(methodReport, options));
 
-         output.push(entry);
-      });
-
-      return output;
+      return entry;
    }
 
    /**
     * Formats a module or class reports methods array.
     *
-    * @param {Array<ClassMethodReport|ModuleMethodReport>}  methodReports - An array of method report instances to
-    *                                                                       format.
+    * @param {MethodReport}   methodReport - A method report instance to format.
     *
-    * @param {object}               options - (Optional) One or more optional parameters passed to the formatter.
-    * @property {string}            methodReport - An entry key found in the method report to output.
+    * @param {object}         options - (Optional) One or more optional parameters passed to the formatter.
+    * @property {string}      methodReport - An entry key found in the method report to output.
     *
-    * @returns {Array<object>}
+    * @returns {object}
     */
-   _formatMethods(methodReports, options)
+   _formatMethod(methodReport, options)
    {
-      const output = [];
+      const entry = {};
 
-      methodReports.forEach((methodReport) =>
+      if (methodReport.name) { entry.name = methodReport.name; }
+      if (methodReport.lineStart) { entry.lineStart = methodReport.lineStart; }
+      if (methodReport.lineEnd) { entry.lineEnd = methodReport.lineEnd; }
+
+      if (Array.isArray(options.methodReport))
       {
-         const entry = {};
-
-         if (methodReport.name) { entry.name = methodReport.name; }
-         if (methodReport.lineStart) { entry.lineStart = methodReport.lineStart; }
-
-         if (Array.isArray(options.methodReport))
+         options.methodReport.forEach((methodEntry) =>
          {
-            options.methodReport.forEach((methodEntry) =>
-            {
-               const entryValue = ObjectUtil.safeAccess(methodReport, methodEntry);
-               if (entryValue) { entry[methodEntry] = entryValue; }
-            });
-         }
+            const entryValue = ObjectUtil.safeAccess(methodReport, methodEntry);
+            if (entryValue) { ObjectUtil.safeSet(entry, methodEntry, entryValue); }
+         });
+      }
 
-         output.push(entry);
-      });
-
-      return output;
+      return entry;
    }
 
    /**
@@ -250,28 +196,58 @@ export default class FormatJSONMinimal
          if (report.filePath) { output.filePath = report.filePath; }
          if (report.srcPath) { output.srcPath = report.srcPath; }
          if (report.srcPathAlias) { output.srcPathAlias = report.srcPathAlias; }
+         if (report.lineStart) { output.lineStart = report.lineStart; }
+         if (report.lineEnd) { output.lineEnd = report.lineEnd; }
 
          if (Array.isArray(options.moduleReport))
          {
             options.moduleReport.forEach((moduleEntry) =>
             {
                const entryValue = ObjectUtil.safeAccess(report, moduleEntry);
-               if (entryValue) { output[moduleEntry] = entryValue; }
+               if (entryValue) { ObjectUtil.safeSet(output, moduleEntry, entryValue); }
             });
          }
 
-         output.classes = this._formatClasses(report.classes, options);
-         output.methods = this._formatMethods(report.methods, options);
+         output.classes = report.classes.map((classReport) => this._formatClass(classReport, options));
+         output.methods = report.methods.map((methodReport) => this._formatMethod(methodReport, options));
       }
       else
       {
          if (report.filePath) { output.filePath = report.filePath; }
          if (report.srcPath) { output.srcPath = report.srcPath; }
          if (report.srcPathAlias) { output.srcPathAlias = report.srcPathAlias; }
+         if (report.lineStart) { output.lineStart = report.lineStart; }
+         if (report.lineEnd) { output.lineEnd = report.lineEnd; }
 
          output.classes = [];
          output.methods = [];
       }
+
+      return output;
+   }
+
+   /**
+    * Formats a project result with minimal metrics.
+    *
+    * @param {ProjectResult}  result - A project result.
+    *
+    * @param {object}         options - (Optional) One or more optional parameters passed to the formatter.
+    * @property {string}      classReport - An entry key found in the class report to output.
+    * @property {string}      methodReport - An entry key found in the method report to output.
+    * @property {string}      moduleReport - An entry key found in the module report to output.
+    *
+    * @returns {object}
+    */
+   _formatProject(result, options)
+   {
+      const output = { modules: [] };
+
+      const reportsAvailable = result.getSetting('serializeReports', false);
+
+      result.reports.forEach((report) =>
+      {
+         output.modules.push(this._formatModule(report, reportsAvailable, options));
+      });
 
       return output;
    }
